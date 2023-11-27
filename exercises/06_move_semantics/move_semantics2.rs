729560cb54a017755f30b0301ff5d6da2161bae5
Henry Zou <henry.zou@tufts.edu>
2023-11-27 17:05:56 -0500

    Finished half

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -5,13 +5,13 @@
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 #[test]
 fn main() {
     let vec0 = vec![22, 44, 66];
+    let vec0_clone = vec0.clone();
 
-    let mut vec1 = fill_vec(vec0);
+    let vec1 = fill_vec(vec0_clone);
 
     assert_eq!(vec0, vec![22, 44, 66]);
     assert_eq!(vec1, vec![22, 44, 66, 88]);
--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -3,21 +3,22 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut trimmed = input.trim();
+    trimmed.to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let composed = input.to_owned() + " world!";
+    composed.to_string()
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let replaced = input.replace("cars", "balloons");
+    replaced.to_string()
 }
 
 #[cfg(test)]
